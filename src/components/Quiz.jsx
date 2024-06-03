a313962252da02f14815d9a63686ba960e919dbb
TanNguyen <minhtan86a@gmail.com>
2024-06-03 11:12:00 +0700

    195 Shuffle answers and check quiz complete

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
+  //Get an element only
   const activeQuestionIndex = userAnswers.length;
+
+  //Check the quiz is complete or not
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     //keep all answers of users into array.
@@ -13,12 +18,27 @@ export default function Quiz() {
     });
   }
 
+  //Check quiz complete
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  //Shuffle answers, should come after check quiz complete
+  //This code is execute if we have a question to display
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]; //not edit original array
+  shuffledAnswers.sort(() => Math.random() - 0.5); //random answers
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
