343358c55af841f15dbe5e2222b33b253d9a4072
TanNguyen <minhtan86a@gmail.com>
2024-06-03 16:28:15 +0700

    201 refactor components

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,46 +4,27 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
   //Get an element only
   //if question not answer yet, then activeQuestionIndex = userAnswers.length
   //else stick to the old question
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
 
   //Check the quiz is complete or not
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   //useCallback hook
   //when activeQuestionIndex change then re-create
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      //user selected answer
-      setAnswerState("answered");
-
-      //keep all answers of users into array.
-      setUserAnswers((prevUserAnswers) => {
-        return [...prevUserAnswers, selectedAnswer];
-      });
-
-      //after 1s to check the answer is right or wrong
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        //set new timer which will only start after this timer expired
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    //keep all answers of users into array.
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
 
   //useCallback hook to handle Skip answer
   //ensure that functions don't get recreated unless dependencies changed
@@ -68,10 +49,7 @@ export default function Quiz() {
       <Question
         //key is used to destroy the old component and create a new one -> unmount and remount it
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
         onSkipAnswer={handleSkipAnswer}
       />
